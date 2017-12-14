7c73ed5732d80c35d64b38b0bf24dce116c216e7
haoflynet <haoflynet@gmail.com>
2017-12-14 09:17:23 +0800

    optimize code

--- callbacks.py
+++ callbacks.py
@@ -1,34 +1,48 @@
-"""
-监听回调函数
-"""
-
+from pynput import keyboard, mouse
 
 scripts = []
 
 
 def on_mouse_move(x, y):
-    pass
+    print(x, y)
 
 
 def on_mouse_click(x, y, button, pressed):
-    pass
+    print(x, y, button, pressed)
 
 
 def on_mouse_scroll(x, y, dx, dy):
-    pass
+    print(x, y, dx, dy)
 
 
 def on_key_press(key):
-    if hasattr(key, 'char'):
-        print(key.char)
+    global scripts
+    if '_name_' in key.__dict__:
+        char = key._name_
+    elif 'char' in key.__dict__:
+        char = key.char
     else:
-        print(key)
+        char = None
+
+    scripts.append(' '.join(['KEY_PRESS', 'None' if char is None else char]))
+
+    if key == keyboard.Key.esc:
+        return False
 
 
 def on_key_release(key):
-    pass
+    global scripts
+    if '_name_' in key.__dict__:
+        char = key._name_
+    elif 'char' in key.__dict__:
+        char = key.char
+    else:
+        char = 'None'
+
+    scripts.append(' '.join(['KEY_RELEASE', 'None' if char is None else char]))
 
 
-
-
+def get_scripts():
+    global scripts
+    return scripts
 
