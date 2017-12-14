bf829ff54d84f33a7fde56bef2e4efb522e906d4
haoflynet <haoflynet@gmail.com>
2017-12-14 18:03:54 +0800

    add time recorder

--- callbacks.py
+++ callbacks.py
@@ -1,5 +1,7 @@
+import time
 from pynput import keyboard, mouse
 
+current_time = 0
 scripts = []
 
 
@@ -16,6 +18,11 @@ def on_mouse_scroll(x, y, dx, dy):
 
 
 def on_key_press(key):
+    """
+    begin的时候设置当前时间
+    :param key:
+    :return:
+    """
     global scripts
     if '_name_' in key.__dict__:
         char = key._name_
@@ -24,7 +31,11 @@ def on_key_press(key):
     else:
         char = None
 
-    scripts.append(' '.join(['KEY_PRESS', 'None' if char is None else char]))
+    print(char)
+
+    global current_time
+    scripts.append(' '.join(['KEY_PRESS', str(int(time.time()) - current_time), 'None' if char is None else char]))
+    current_time = int(time.time())
 
     if key == keyboard.Key.esc:
         return False
@@ -39,10 +50,16 @@ def on_key_release(key):
     else:
         char = 'None'
 
-    scripts.append(' '.join(['KEY_RELEASE', 'None' if char is None else char]))
+    global current_time
+    scripts.append(' '.join(['KEY_RELEASE', str(int(time.time()) - current_time), 'None' if char is None else char]))
+    current_time = int(time.time())
+
+
+def set_current_time():
+    global current_time
+    current_time = int(time.time())
 
 
 def get_scripts():
     global scripts
     return scripts
-
