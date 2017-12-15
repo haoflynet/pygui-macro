43c3e0088bbf63229465187befd48ae9348fda9f
haoflynet <haoflynet@gmail.com>
2017-12-15 23:44:14 +0800

    bug fix

--- listener.py
+++ listener.py
@@ -3,30 +3,38 @@
 """
 from pynput import keyboard, mouse
 
-from . import callbacks
-
+import callbacks
 
 mouse_events = {
-    'mouse_move': callbacks.on_mouse_move,
-    'mouse_click': callbacks.on_mouse_click,
-    'mouse_scroll': callbacks.on_mouse_scroll
+    'on_move': callbacks.on_mouse_move,
+    'on_click': callbacks.on_mouse_click,
+    'on_scroll': callbacks.on_mouse_scroll
 }
 
 keyboard_events = {
-    'key_press': callbacks.on_key_press,
-    'key_release': callbacks.on_key_release
+    'on_press': callbacks.on_key_press,
+    'on_release': callbacks.on_key_release
 }
+
+
+class StopException(Exception):
+    pass
 
 
 def listen(exclude_events=None):
     for exclude_event in exclude_events:
-        del mouse_events[exclude_event]
-        del keyboard_events[exclude_event]
+        if exclude_event in mouse_events:
+            del mouse_events[exclude_event]
+        if exclude_event in keyboard_events:
+            del keyboard_events[exclude_event]
 
     with keyboard.Listener(**keyboard_events) as keyboard_listener:
         with mouse.Listener(**mouse_events) as mouse_listener:
-            keyboard_listener.join()
-            mouse_listener.join()
+            try:
+                keyboard_listener.join()
+                mouse_listener.join()
+            except StopException as e:
+                print('ok?')
 
 
 if __name__ == '__main__':
