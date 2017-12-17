4ff88b6f7af407fe6b28668f3a8823acd22d6d1d
haoflynet <haoflynet@gmail.com>
2017-12-17 23:14:23 +0800

    bug fix

--- gui-macro.py
+++ gui-macro.py
@@ -22,10 +22,11 @@ def main():
     parser_record.add_argument('-oa', '--original_auto', action='store_true', dest='original_auto', help='自动记录初始化坐标 (default: False)')
     parser_record.add_argument('-s', '--start_key', dest='start_key', help='开始记录快捷键 (default: None)')
     parser_record.add_argument('-e', '--end_key', dest='end_key', help='结束记录快捷键 (default: Esc')
-    parser_record.add_argument('-ex', '--include_events', action='append', dest='include_events', help='需要记录事件 (default: [])')
+    parser_record.add_argument('-ie', '--include_events', action='append', dest='include_events', help='需要记录事件 (default: [])')
     parser_record.add_argument('-d', '--destination', dest='destination', help='输出文件夹 (default: ./)')
     parser_record.add_argument('-f', '--file', dest='file', help='输出文件 (default: script')
     parser_record.add_argument('-c', '--continue', action='store_true', dest='is_continue', help='断点续记 (default: false)')
+    parser_record.add_argument('-ar', '--auto_release', action='store_true', dest='auto_release', help='自动释放按键 (default: true)')
 
     parser_run = subparsers.add_parser('exec', help='执行脚本')
     parser_run.add_argument('-s', '--start_key', dest='start_key', help='开始执行快捷键 (default: None)')
--- recoder.py
+++ recoder.py
@@ -15,8 +15,9 @@ class Recoder:
     original = (0, 0)
     original_auto = False
     is_continue = False
+    auto_release = True
 
-    def __init__(self, start_key, end_key, include_events, destination, file, original, original_auto, is_continue):
+    def __init__(self, start_key, end_key, include_events, destination, file, original, original_auto, is_continue, auto_release):
         if start_key is not None:
             self.start_key = start_key
         if end_key is not None:
@@ -36,9 +37,11 @@ class Recoder:
             self.original_auto = original_auto
         if is_continue is not None:
             self.is_continue = is_continue
+        if auto_release is not None:
+            self.auto_release = auto_release
 
     def handle(self):
-        Listener(self.include_events).listen()
+        Listener(self.include_events, self.auto_release).listen()
 
     def write(self):
         """
@@ -49,8 +52,9 @@ class Recoder:
         device_info = ':DEVICE: {} {}'.format(
             platform.system(), platform.release()
         )
-        scripts = [device_info, ':START'] + Callbacks.get_scripts() + [':END']
 
-        fp = codecs.open('test', 'w', 'utf-8')
+        scripts = [device_info, 'True' if self.auto_release else 'False', ':START'] + Callbacks.get_scripts() + [':END']
+
+        fp = codecs.open(os.path.join(self.destination, self.file), 'w', 'utf-8')
         fp.write(' \n'.join(scripts))
         fp.close()
