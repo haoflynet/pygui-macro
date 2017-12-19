b79388d91862ab0ef4bfa34de0e0a0b1f93f8115
haoflynet <haoflynet@gmail.com>
2017-12-19 18:34:25 +0800

    remove unused arguments

--- gui-macro.py
+++ gui-macro.py
@@ -1,6 +1,3 @@
-"""
-TODO： 手动编辑、if语句、while语句
-"""
 import os
 import argparse
 
@@ -14,25 +11,22 @@ def main():
     :return:
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--version', help='打印版本信息')
+    parser.add_argument('-v', '--version', help='print version info')
     subparsers = parser.add_subparsers(dest='subparser')
 
-    parser_record = subparsers.add_parser('record', help='记录脚本')
-    parser_record.add_argument('-o', '--original', dest='original', help='初始坐标 (default: 0, 0)')
-    parser_record.add_argument('-oa', '--original_auto', action='store_true', dest='original_auto', help='自动记录初始化坐标 (default: False)')
-    parser_record.add_argument('-s', '--start_key', dest='start_key', help='开始记录快捷键 (default: None)')
-    parser_record.add_argument('-e', '--end_key', dest='end_key', help='结束记录快捷键 (default: Esc')
-    parser_record.add_argument('-ie', '--include_events', action='append', dest='include_events', help='需要记录事件 (default: [])')
-    parser_record.add_argument('-d', '--destination', dest='destination', help='输出文件夹 (default: ./)')
-    parser_record.add_argument('-f', '--file', dest='file', help='输出文件 (default: script')
-    parser_record.add_argument('-c', '--continue', action='store_true', dest='is_continue', help='断点续记 (default: false)')
-    parser_record.add_argument('-ar', '--auto_release', action='store_true', dest='auto_release', help='自动释放按键 (default: true)')
+    parser_record = subparsers.add_parser('record', help='record and generate macro script')
+    parser_record.add_argument('-d', '--delay', dest='delay', help='delay time to record (default: 3)')
+    parser_record.add_argument('-o', '--original', dest='original', help='original coordinate (default: 0, 0)')
+    parser_record.add_argument('-oa', '--original_auto', action='store_true', dest='original_auto', help='set original coordinate automatically (default: False)')
+    parser_record.add_argument('-e', '--end_key', dest='end_key', help='end record hot key (default: Esc')
+    parser_record.add_argument('-ie', '--include_events', action='append', dest='include_events', help='which event be recorded (default: [])')
+    parser_record.add_argument('-f', '--file', dest='file', help='macro script filename (default: script')
+    parser_record.add_argument('-c', '--continue', action='store_true', dest='is_continue', help='continuee (default: false)')
+    parser_record.add_argument('-ar', '--auto_release', action='store_true', dest='auto_release', help='whether auto release the key (default: true)')
 
-    parser_run = subparsers.add_parser('exec', help='执行脚本')
-    parser_run.add_argument('-s', '--start_key', dest='start_key', help='开始执行快捷键 (default: None)')
-    parser_run.add_argument('-e', '--end_key', dest='end_key', help='结束执行快捷键 (default: Esc')
-    parser_run.add_argument('-f', '--file', dest='file', help='指定脚本文件执行 (default: script)')
-    parser_run.add_argument('-ar', '--auto_release', action='store_true', dest='is_auto_release', help='自动释放按键 (default: true)')
+    parser_run = subparsers.add_parser('run', help='run the macro script')
+    parser_record.add_argument('-d', '--delay', dest='delay', help='delay time to run (default: 3)')
+    parser_run.add_argument('-f', '--file', dest='file', help='specify the macro script filename (default: script)')
 
     kwargs = vars(parser.parse_args())
     command = kwargs.pop('subparser')
@@ -46,7 +40,7 @@ def main():
         kwargs.pop('version')
         if command == 'record':
             Recoder(**kwargs).handle()
-        elif command == 'exec':
+        elif command == 'run':
             Runner(**kwargs).handle()
 
 
